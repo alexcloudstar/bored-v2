5a4fc900f9f35d7b6624fe11abbc3bd7d9f3840b
alexcloudstar <alexcloudstar@gmail.com>
2023-06-01 23:41:38 +0300

    configure build options

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,28 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
-import path from 'path';
+import viteCompression from 'vite-plugin-compression';
 
-// https://vitejs.dev/config/
+import { defineConfig } from 'vite';
+
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), viteCompression()],
   resolve: {
     alias: {
       src: '/src',
+    },
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id
+              .toString()
+              .split('node_modules/')[1]
+              .split('/')[0]
+              .toString();
+          }
+        },
+      },
     },
   },
 });
