1b2ae81c8e97385ff118b3959d090d6dee546405
alexcloudstar <alexcloudstar@gmail.com>
2023-06-01 22:45:22 +0300

    set store for activity

--- src/constants/store.ts
+++ src/constants/store.ts
@@ -10,3 +10,25 @@ export const useDarkModeStore = create<DarkModeState>(set => ({
   darkMode: getLocalStorage('darkMode') === 'true',
   toggleDarkMode: () => set(state => ({ darkMode: !state.darkMode })),
 }));
+
+type ActivityState = {
+  activity: {
+    activity: string;
+    type: string;
+    participants: number;
+    price: number;
+    accessibility: number;
+  };
+  setActivity: (activity: ActivityState['activity']) => void;
+};
+
+export const useActivityStore = create<ActivityState>(set => ({
+  activity: {
+    activity: '',
+    type: '',
+    participants: 0,
+    price: 0,
+    accessibility: 0,
+  },
+  setActivity: (activity: ActivityState['activity']) => set({ activity }),
+}));
