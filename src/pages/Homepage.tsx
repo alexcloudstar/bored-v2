e7c7fa67697d6b3f7c472c06e9db24685f7f8a8b
alexcloudstar <alexcloudstar@gmail.com>
2023-06-01 11:24:39 +0300

    feat: Configure a bare store for zustand

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,10 +1,31 @@
-import { Col, Row } from 'antd';
+import { Button, Col, Row } from 'antd';
+import { PlusOutlined, MinusOutlined } from '@ant-design/icons';
+import { useBearStore } from '../constants/store';
 
 const Homepage = () => {
+  const { bears, increase, decrease } = useBearStore(state => state);
+
+  const increaseBears = () => increase(1);
+  const decreaseBears = () => decrease(1);
+
   return (
     <>
       <Row>
-        <Col span={24}>col-12</Col>
+        <Col span={24}>Current number of 🧸: {bears}</Col>
+        <Col span={24}>
+          <Button
+            type='primary'
+            icon={<PlusOutlined />}
+            size='large'
+            onClick={increaseBears}
+          />
+          <Button
+            type='primary'
+            icon={<MinusOutlined />}
+            size='large'
+            onClick={decreaseBears}
+          />
+        </Col>
       </Row>
     </>
   );
